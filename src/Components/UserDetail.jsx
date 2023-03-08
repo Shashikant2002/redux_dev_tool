9de5fc6b799beb1535b54d88cce0cb7050f436c1
Shashikant <shashikant384443>
2023-03-08 21:28:44 +0530

    redux tool kit

---
+++ src/Components/UserDetail.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fackUserData } from "../pi/api";
+import { addUser, removeUser } from "../redux/store/slices/userSlice";
+
+const UserDetail = () => {
+  const dispatch = useDispatch();
+  const names = useSelector((state) => state.users);
+
+  const addNewUser = (name) => {
+    dispatch(addUser(name));
+  };
+
+  const removeEle = (i) => {
+    dispatch(removeUser(i));
+  };
+
+  return (
+    <>
+      <div className="userDetail d-flex justify-content-center flex-column align-items-center">
+        <div className="mainUsers d-flex justify-content-center w-100 flex-wrap">
+          {names.map((ele, i) => {
+            return (
+              <div
+                key={i}
+                className="user mt-2 w-15 border text-center p-4 mx-3 my-4"
+              >
+                <h3>{ele}</h3>
+                <button onClick={() => removeEle(i)} className="py-1 px-5 btn btn btn-danger">
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          onClick={() => addNewUser(fackUserData())}
+          className="mt-5 text-center py-1 px-5 btn btn-primary"
+        >
+          Add User
+        </button>
+        <button
+          onClick={() => addNewUser(fackUserData())}
+          className="mt-1 text-center py-1 px-5 btn btn-danger"
+        >
+          Remove All User
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default UserDetail;
---
+++ src/redux/store/slices/userSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice, createAction } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: [],
+  reducers: {
+    addUser(state, action) {
+      state.push(action.payload)
+    },
+    removeUser(state, action) {
+        const name = action.payload;
+        state.splice(name, 1);
+    },
+    clearAllUser(state, action) {},
+  },
+});
+
+export default userSlice.reducer;
+export const {addUser, removeUser, clearAllUser} = userSlice.actions;
